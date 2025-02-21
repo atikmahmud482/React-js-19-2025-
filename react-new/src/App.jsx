e9f80f8c1075a753b1e5fd706a6546ee6567c033
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-21 22:42:21 +0600

    state managment

--- react-new/src/App.jsx
+++ react-new/src/App.jsx
@@ -7,7 +7,9 @@ const Card = ({ title }) => {
     <div className="card">
       <h2>{title}</h2>
 
-      <button onClick={() => setHasLiked(true)}>Like</button>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
     </div>
   );
 };
