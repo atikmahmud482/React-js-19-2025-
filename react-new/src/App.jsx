e81f31e90c5f66bdd2978ad5d820a1ca06156248
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-22 15:20:50 +0600

    useeffect

--- react-new/src/App.jsx
+++ react-new/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} <br /> {count}
+      </h2>
 
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? "❤️" : "🤍"}
