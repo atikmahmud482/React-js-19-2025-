7adf40d0390a9f83edbfb316132d14183da48116
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-01 00:16:39 +0600

    Rendering List

--- react-dev/src/App.jsx
+++ react-dev/src/App.jsx
@@ -1,19 +1,42 @@
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
+// Displaying Data
 
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt=""
-        style={{ width: user.imageSize, height: user.imageSize }}
-      />
-    </>
-  );
+// const user = {
+//   name: "Hedy Lamarr",
+//   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+//   imageSize: 90,
+// };
+
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt=""
+//         style={{ width: user.imageSize, height: user.imageSize }}
+//       />
+//     </>
+//   );
+// }
+
+// Rendering Lists
+
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItem = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}>
+      {product.title}
+    </li>
+  ));
+  return <ul>{listItem}</ul>;
 }
