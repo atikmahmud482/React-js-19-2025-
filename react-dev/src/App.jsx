19fbcf6b2c676fa30fd77a7f3f4a19d37cfabf37
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-01 01:07:29 +0600

    Responding to Events

--- react-dev/src/App.jsx
+++ react-dev/src/App.jsx
@@ -22,21 +22,45 @@
 
 // Rendering Lists
 
-const products = [
-  { title: "Cabbage", isFruit: false, id: 1 },
-  { title: "Garlic", isFruit: false, id: 2 },
-  { title: "Apple", isFruit: true, id: 3 },
-];
+// const products = [
+//   { title: "Cabbage", isFruit: false, id: 1 },
+//   { title: "Garlic", isFruit: false, id: 2 },
+//   { title: "Apple", isFruit: true, id: 3 },
+// ];
 
-export default function ShoppingList() {
-  const listItem = products.map((product) => (
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? "magenta" : "darkgreen",
-      }}>
-      {product.title}
-    </li>
-  ));
-  return <ul>{listItem}</ul>;
+// export default function ShoppingList() {
+//   const listItem = products.map((product) => (
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? "magenta" : "darkgreen",
+//       }}>
+//       {product.title}
+//     </li>
+//   ));
+//   return <ul>{listItem}</ul>;
+// }
+
+// Updating the Screen
+
+import { useState } from "react";
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
