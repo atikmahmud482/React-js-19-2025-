a4ef56773b5093896064eabf57221c93f3c0fb71
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-03 23:44:03 +0600

    Read props inside the child component

--- react-dev/src/Apptwo.jsx
+++ react-dev/src/Apptwo.jsx
@@ -81,18 +81,60 @@
 // }
 
 //Familiar props
-function Avatar() {
+// function Avatar() {
+//   return (
+//     <img
+//       className="avatar"
+//       src="https://i.imgur.com/1bX5QH6.jpg"
+//       alt="Lin Lanying"
+//       width={100}
+//       height={100}
+//     />
+//   );
+// }
+
+// export default function Profile() {
+//   return <Avatar />;
+// }
+
+import { getImageUrl } from "./utils.js";
+
+function Avatar({ person, size }) {
   return (
     <img
       className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
     />
   );
 }
 
 export default function Profile() {
-  return <Avatar />;
+  return (
+    <div>
+      <Avatar
+        size={100}
+        person={{
+          name: "Katsuko Saruhashi",
+          imageId: "YfeOqp2",
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: "Aklilu Lemma",
+          imageId: "OKS67lh",
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{
+          name: "Lin Lanying",
+          imageId: "1bX5QH6",
+        }}
+      />
+    </div>
+  );
 }
