f1d0e117891400da0d7b0e72ca6397b34f49fd80
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-04 22:50:42 +0600

    Conditionally returning JSX

--- react-dev/src/Apptwo.jsx
+++ react-dev/src/Apptwo.jsx
@@ -345,3 +345,23 @@
 //     <Avatar parson={{ name: "Lin Lanying", imageId: "1bX5QH6" }} size={100} />
 //   );
 // }
+
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✅</li>;
+  }
+  return <li className="item">{name}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
