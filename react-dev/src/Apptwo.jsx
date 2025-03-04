5adc0cbd8c5cf7717d1e740881bb4f8f3a0ed623
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-04 22:44:44 +0600

    Adjust the image size based on a prop

--- react-dev/src/Apptwo.jsx
+++ react-dev/src/Apptwo.jsx
@@ -323,25 +323,25 @@
 // }
 
 //Familiar props
-function Avatar() {
-  return (
-    <img
-      className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
-    />
-  );
-}
+// function Avatar() {
+//   return (
+//     <img
+//       className="avatar"
+//       src="https://i.imgur.com/1bX5QH6.jpg"
+//       alt="Lin Lanying"
+//       width={100}
+//       height={100}
+//     />
+//   );
+// }
 
 //passing props to a component
 // export default function Profile() {
 //   return <Avatar />;
 // }
 
-export default function Profile() {
-  return (
-    <Avatar parson={{ name: "Lin Lanying", imageId: "1bX5QH6" }} size={100} />
-  );
-}
+// export default function Profile() {
+//   return (
+//     <Avatar parson={{ name: "Lin Lanying", imageId: "1bX5QH6" }} size={100} />
+//   );
+// }
