a5f119709b1041bea20150652f5ef238d21ff13a
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-04 22:59:03 +0600

    Show an icon for incomplete items with ? :

--- react-dev/src/Apptwo.jsx
+++ react-dev/src/Apptwo.jsx
@@ -346,22 +346,76 @@
 //   );
 // }
 
-function Item({ name, isPacked }) {
-  if (isPacked) {
-    return <li className="item">{name} ✅</li>;
-  }
-  return <li className="item">{name}</li>;
-}
+//Conditionally returning JSX
+// function Item({ name, isPacked }) {
+//   if (isPacked) {
+//     return <li className="item">{name} ✅</li>;
+//   }
+//   return <li className="item">{name}</li>;
+// }
 
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
-      </ul>
-    </section>
-  );
-}
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item isPacked={true} name="Space suit" />
+//         <Item isPacked={true} name="Helmet with a golden leaf" />
+//         <Item isPacked={false} name="Photo of Tam" />
+//       </ul>
+//     </section>
+//   );
+// }
+
+// function Item({ name, isPacked }) {
+//   let itemContent = name;
+//   if (isPacked) {
+//     itemContent = <del>{name + " ✅"}</del>;
+//   }
+//   return <li className="item">{itemContent}</li>;
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item isPacked={true} name="Space suit" />
+//         <Item isPacked={true} name="Helmet with a golden leaf" />
+//         <Item isPacked={false} name="Photo of Tam" />
+//       </ul>
+//     </section>
+//   );
+// }
+
+//Show an icon for incomplete items with ? :
+
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked ? '✅' : '❌'}
+//     </li>
+//   );
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item
+//           isPacked={true}
+//           name="Space suit"
+//         />
+//         <Item
+//           isPacked={true}
+//           name="Helmet with a golden leaf"
+//         />
+//         <Item
+//           isPacked={false}
+//           name="Photo of Tam"
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
