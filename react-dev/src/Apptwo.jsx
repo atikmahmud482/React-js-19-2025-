b042a1a0cf81159ba7c4452cc1bcd2c8d80109c9
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-08 23:44:01 +0600

    react

--- react-dev/src/Apptwo.jsx
+++ react-dev/src/Apptwo.jsx
@@ -630,3 +630,12 @@
 // export default function Clock({ color, time }) {
 //   return <h1 style={{ color: color }}>{time}</h1>;
 // }
+
+//Recap
+// To pass props, add them to the JSX, just like you would with HTML attributes.
+// To read props, use the function Avatar({ person, size }) destructuring syntax.
+// You can specify a default value like size = 100, which is used for missing and undefined props.
+// You can forward all props with <Avatar {...props} /> JSX spread syntax, but don’t overuse it!
+// Nested JSX like <Card><Avatar /></Card> will appear as Card component’s children prop.
+// Props are read-only snapshots in time: every render receives a new version of props.
+// You can’t change props. When you need interactivity, you’ll need to set state.
