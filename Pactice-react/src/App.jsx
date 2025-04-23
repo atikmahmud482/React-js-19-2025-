b25f6a4510f86731dd5c2e63b18f6838247f31fd
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-23 23:22:30 +0600

    pages added

--- Pactice-react/src/App.jsx
+++ Pactice-react/src/App.jsx
@@ -8,12 +8,6 @@ import About from "./pages/About";
 import Contact from "./pages/Contract";
 import Footer from "./components/Footer";
 import ErrorBoundary from "./ErrorBoundary";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Products from "./pages/Products";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
 
 const App = () => {
   return (
--- Pactice-react/src/pages/Home.jsx
+++ Pactice-react/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Banner from "../components/Banner";
+import CardSection from "../components/CardSection";
 
 const Home = () => {
   return (
     <>
       <Banner />
-      <h2 className="text-center mt-10 text-xl font-bold">
-        Welcome to CloudCore!
-      </h2>
+      <div>
+        <h2 className="text-center mt-10 text-xl font-bold">
+          Welcome to CloudCore!
+        </h2>
+        <CardSection></CardSection>
+      </div>
     </>
   );
 };
