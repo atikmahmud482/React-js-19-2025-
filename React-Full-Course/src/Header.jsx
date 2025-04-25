6d0b0350f6fb9723241afecb3c2c3fadaa178aed
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-25 21:16:40 +0600

    header components

---
+++ React-Full-Course/src/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
