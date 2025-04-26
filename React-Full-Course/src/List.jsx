7e335f62fb2bb589b3000991f20bae8fb726cd95
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-26 22:29:32 +0600

    render lists

--- React-Full-Course/src/List.jsx
+++ React-Full-Course/src/List.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 function List() {
-  const fruits = ["apple", "banana", "orange"];
+  const fruits = [
+    { key: 1, name: "Apple", calories: 95 },
+    { key: 2, name: "Banana", calories: 105 },
+    { key: 3, name: "Orange", calories: 62 },
+    { key: 4, name: "Mango", calories: 150 },
+  ];
 
-  const listItems = fruits.map((fruit) => <li>{fruit}</li>);
+  fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical sorting
+
+  const listItems = fruits.map((fruit) => (
+    <li key={fruit.key}>
+      {fruit.name}: &nbsp; <b>{fruit.calories}</b>
+    </li>
+  ));
 
   return <ol>{listItems}</ol>;
 }
