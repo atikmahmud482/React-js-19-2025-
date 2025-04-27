6978e3113cb24418a4e7f88e231620b565287b77
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-27 23:26:56 +0600

    click event

--- React-Full-Course/src/Buttons.jsx
+++ React-Full-Course/src/Buttons.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 function Button() {
-  const handleClick = () => {
-    console.log("OUTCH!");
+  let count = 0;
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++;
+      console.log(`${name} you clicked me ${count} times!`);
+    } else {
+      console.log(`${name} stop clicking me!`);
+    }
   };
-  const handleClick2 = (name) => {
-    console.log(`${name} stop clicking me!`);
-  };
+  // const handleClick2 = (name) => {
+  //   console.log(`${name} stop clicking me!`);
+  // };
   return (
     <div>
-      <button onClick={() => handleClick2("Bro")}>Click me</button>
+      <button onClick={() => handleClick("Bro")}>Click me</button>
     </div>
   );
 }
