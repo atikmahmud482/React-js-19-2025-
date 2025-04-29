a597fe3979ec3f1058d08dcad2a6a9a852b92428
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-29 23:54:36 +0600

    onChange

--- React-Full-Course/src/MyComponent.jsx
+++ React-Full-Course/src/MyComponent.jsx
@@ -4,6 +4,8 @@ function MyComponent() {
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
   const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState();
+  const [shipping, setShipping] = useState();
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -16,6 +18,14 @@ function MyComponent() {
   function handleCommentChange(e) {
     setComment(e.target.value);
   }
+
+  function handlePaymentChange(e) {
+    setPayment(e.target.value);
+  }
+
+  function handleShippingChange(e) {
+    setShipping(e.target.value);
+  }
   return (
     <div>
       <input type="text" value={name} onChange={handleNameChange} />
@@ -24,6 +34,32 @@ function MyComponent() {
       <p>Quantity: {quantity}</p>
       <textarea value={comment} onChange={handleCommentChange} id=""></textarea>
       <p>Comment: {comment}</p>
+      <select name="" value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an Option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">Mastercard</option>
+      </select>
+      <p>Payment</p>
+      <label>
+        <input
+          type="radio"
+          value="Pick Up"
+          checked={shipping === "Pick Up"}
+          onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>{" "}
+      <br />
+      <label htmlFor="">
+        <input
+          type="radio"
+          value="Delivery"
+          checked={shipping === "Delivery"}
+          onChange={handleShippingChange}
+        />
+        Delivery
+      </label>
+      <p> shipping: {shipping} </p>
     </div>
   );
 }
