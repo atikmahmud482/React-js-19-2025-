b7b6d933a1163af1b0ce8085cbd5be061d009f1c
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-29 23:41:51 +0600

    onChange

--- React-Full-Course/src/MyComponent.jsx
+++ React-Full-Course/src/MyComponent.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function MyComponent() {
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
+  const [comment, setComment] = useState("");
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -11,12 +12,18 @@ function MyComponent() {
   function handleQuantityChange(e) {
     setQuantity(e.target.value);
   }
+
+  function handleCommentChange(e) {
+    setComment(e.target.value);
+  }
   return (
     <div>
       <input type="text" value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
       <input type="Number" value={quantity} onChange={handleQuantityChange} />
       <p>Quantity: {quantity}</p>
+      <textarea value={comment} onChange={handleCommentChange} id=""></textarea>
+      <p>Comment: {comment}</p>
     </div>
   );
 }
