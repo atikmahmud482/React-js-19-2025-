5a08c7bd83ca6ebe74951a694f815142ca5355f7
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-29 22:19:10 +0600

    useState

--- React-Full-Course/src/MyComponents.jsx
+++ React-Full-Course/src/MyComponents.jsx
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 
 function MyComponents() {
   const [name, setName] = useState("atik");
+  const [age, setAge] = useState(0);
 
   const updateName = () => {
     setName("Mahmud");
+  };
+  const updateAge = () => {
+    setAge(age + 1);
   };
 
   return (
     <div>
       <p>Name: {name}</p>
       <button onClick={updateName}>Set Name</button>
+      <p>Age: {age}</p>
+      <button onClick={updateAge}>Set Age</button>
     </div>
   );
 }
