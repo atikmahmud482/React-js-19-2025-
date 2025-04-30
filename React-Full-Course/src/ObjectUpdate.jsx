85b8fdfe2aa9a3788812f83b06d19a892d9e6b7d
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-30 22:54:14 +0600

    object-updater

--- React-Full-Course/src/ObjectUpdate.jsx
+++ React-Full-Course/src/ObjectUpdate.jsx
@@ -7,12 +7,21 @@ function ObjectUpdate() {
     year: 2020,
     color: "black",
   });
+  function handleBrandChange() {}
+
+  function handleModelChange() {}
+
+  function handleYearChange() {}
+
+  function handleColorChange() {}
+
   return (
     <div>
       <p className="object-update">
         Your favorite car is: {car.brand}-{car.model}-{car.color}-{car.year}{" "}
       </p>
       <input type="number" value={car.year} /> <br />
+      <input type="text" value={car} />
     </div>
   );
 }
