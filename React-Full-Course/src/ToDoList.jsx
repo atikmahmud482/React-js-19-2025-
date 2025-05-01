ea7cb3bfc5980a49c6beaef3f79aa62ab66357a3
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-01 23:31:04 +0600

    ToDoList

--- React-Full-Course/src/ToDoList.jsx
+++ React-Full-Course/src/ToDoList.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoList() {
-  return <div>ToDoList</div>;
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(e) {}
+
+  function addTask() {}
+
+  function removeTask(index) {}
+
+  function noveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+  return (
+    <div className="todo-list">
+      <h1>To-Do-List</h1>
+    </div>
+  );
 }
 
 export default ToDoList;
