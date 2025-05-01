b26277c9fe5109ccad7a353228c2f8bb6f48f760
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-01 23:05:47 +0600

    array Of object

--- React-Full-Course/src/ArrayOfObject.jsx
+++ React-Full-Course/src/ArrayOfObject.jsx
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 
 function ArrayOfObject() {
   const [cars, setCars] = useState([]);
-  const [carYear, setCarYear] = useState();
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
+
+  function handleAddCar() {}
+
+  function handleRemoveCar(index) {}
+
+  function handleYearChange(event) {}
+
+  function handleMakeChange(event) {}
+
+  function handleModelChange(event) {}
+
   return <div>ArrayOfObject</div>;
 }
 
