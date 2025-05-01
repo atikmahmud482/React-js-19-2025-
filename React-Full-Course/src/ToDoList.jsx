2b33b93f3f9e2a5adaa468ed8af69cca3223d36a
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-01 23:43:09 +0600

    ToDoList

--- React-Full-Course/src/ToDoList.jsx
+++ React-Full-Course/src/ToDoList.jsx
@@ -1,21 +1,48 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [task, setTask] = useState([]);
+  const [tasks, setTask] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  function handleInputChange(e) {}
+  function handleInputChange(e) {
+    setNewTask(e.target.value);
+  }
 
   function addTask() {}
 
-  function removeTask(index) {}
+  function deleteTask(index) {}
 
   function noveTaskUp(index) {}
 
   function moveTaskDown(index) {}
   return (
     <div className="todo-list">
-      <h1>To-Do-List</h1>
+      <h2>To-Do-List</h2>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTask} className="add-button">
+          Add
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => {
+          <li key={index}>
+            <span className="text"> {task}</span>
+            <button className="delete-Button" onClick={() => deleteTask(index)}>
+              Delete Button
+            </button>
+            <button className="delete-Button" onClick={() => deleteTask(index)}>
+              Delete Button
+            </button>
+          </li>;
+        })}
+      </ul>
     </div>
   );
 }
