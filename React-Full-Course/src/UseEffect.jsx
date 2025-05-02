2765b7d13f24bf2bfcead97ae16908eee3f28867
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-02 23:14:51 +0600

    useeffect

--- React-Full-Course/src/UseEffect.jsx
+++ React-Full-Course/src/UseEffect.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 
 function UseEffect() {
-  const [count, setCount] = useState();
-  return <div>UseEffect</div>;
+  const [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount((c) => c + 1);
+  }
+  return (
+    <>
+      <p> Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+    </>
+  );
 }
 
 export default UseEffect;
