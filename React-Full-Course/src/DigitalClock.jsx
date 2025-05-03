6a3a350b51cce84e6bb67268e2793f1f74b47a45
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-03 21:31:51 +0600

    DigitalClock

--- React-Full-Course/src/DigitalClock.jsx
+++ React-Full-Course/src/DigitalClock.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function formatTime(date) {
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(
+      seconds
+    )} ${meridiem}`;
+  }
+
+  function padZero(num) {
+    return num < 10 ? "0" + num : "" + num;
+  }
+
   return (
     <div className="clock-container">
       <div className="clock">
-        <span>00:00:00</span>
+        <span>{formatTime(time)}</span>
       </div>
     </div>
   );
