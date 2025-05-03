670bd4cc0ebf7821e7c869d0901e092bf3c004aa
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-03 14:58:32 +0600

    useeffect

--- React-Full-Course/src/UseEffect.jsx
+++ React-Full-Course/src/UseEffect.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function UseEffect() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("red");
+  useEffect(() => {
+    document.title = `Count: ${count} ${color}`;
+  }, [count, color]);
 
   function addCount() {
     setCount((c) => c + 1);
   }
+  function subtractCount() {
+    setCount((c) => c - 1);
+  }
+
+  function colorChange() {
+    setColor((c) => (c === "red" ? "blue" : "red"));
+  }
+
   return (
     <>
-      <p> Count: {count}</p>
+      <p style={{ color: color }}> Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button> <br />
+      <button onClick={colorChange}>Color Change</button>
     </>
   );
 }
