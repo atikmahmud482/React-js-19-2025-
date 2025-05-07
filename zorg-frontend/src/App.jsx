308fb8d6113e64ceaf8b58b63637d527ec81936a
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-07 17:03:47 +0600

    worksSection done

--- zorg-frontend/src/App.jsx
+++ zorg-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import WorksSection from "./components/WorksSection";
 
 function App() {
   return (
     <div className="bg-black min-h-screen">
       <Header />
       <HeroSection />
+      <WorksSection />
     </div>
   );
 }
---
+++ zorg-frontend/src/components/WorksSection.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import work1 from "../assets/image1.png";
+import work2 from "../assets/image3.png";
+import work3 from "../assets/image2.png";
+
+const WorksSection = () => {
+  return (
+    <section className="w-full py-16 bg-transparent flex justify-center items-center">
+      <div className="max-w-[1140px] w-full flex flex-col md:flex-row justify-between items-center gap-8 px-4">
+        {/* Work 1 */}
+        <img
+          src={work1}
+          alt="Work 1"
+          className="w-full md:w-[30%] h-auto object-cover rounded-[24px] border-[5px] border-white"
+        />
+        {/* Work 2 */}
+        <img
+          src={work2}
+          alt="Work 2"
+          className="w-full md:w-[30%] h-auto object-cover rounded-[24px] border-[5px] border-white"
+        />
+        {/* Work 3 */}
+        <img
+          src={work3}
+          alt="Work 3"
+          className="w-full md:w-[30%] h-auto object-cover rounded-[24px] border-[5px] border-white"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default WorksSection;
