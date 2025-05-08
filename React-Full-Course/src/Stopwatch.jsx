38015fdae39d2e24e6f576a1638e862c1bc0d403
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-08 22:09:52 +0600

    Stopwatch

--- React-Full-Course/src/Stopwatch.jsx
+++ React-Full-Course/src/Stopwatch.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+
   return <div>Stopwatch</div>;
 }
 
