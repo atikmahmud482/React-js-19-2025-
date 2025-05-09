799cb1079c9e09b451c7895b8b5fdce5e7fe64a4
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-09 23:25:25 +0600

    Stopwatch done

--- React-Full-Course/src/Stopwatch.jsx
+++ React-Full-Course/src/Stopwatch.jsx
@@ -1,3 +1,4 @@
+import { string } from "prop-types";
 import React, { useEffect, useRef, useState } from "react";
 
 function Stopwatch() {
@@ -30,9 +31,16 @@ function Stopwatch() {
   }
   function formatTime() {
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let minuits = Math.floor(elapsedTime / (1000 * 60) / 60);
+    let minutes = Math.floor(elapsedTime / (1000 * 60) / 60);
     let seconds = Math.floor((elapsedTime / 1000) % 60);
-    return `00:00:00`;
+    let milliseconds = Math.floor(elapsedTime % 1000) / 10;
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    return `${minutes}: ${seconds}: ${milliseconds}`;
   }
   return (
     <div className="stopwatch">
