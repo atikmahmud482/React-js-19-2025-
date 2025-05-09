83c6b6bdd317cd65ba81dccd834f7f6f4dd939ab
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-09 22:52:07 +0600

    Stopwatch style

--- React-Full-Course/src/Stopwatch.jsx
+++ React-Full-Course/src/Stopwatch.jsx
@@ -19,15 +19,17 @@ function Stopwatch() {
   return (
     <div className="stopwatch">
       <div className="display">{formatTime()} </div>
-      <button onClick={start} className="start-button">
-        Start
-      </button>
-      <button onClick={stop} className="stop-button">
-        Stop
-      </button>
-      <button onClick={reset} className="reset-button">
-        Reset
-      </button>
+      <div className="controls">
+        <button onClick={start} className="start-button">
+          Start
+        </button>
+        <button onClick={stop} className="stop-button">
+          Stop
+        </button>
+        <button onClick={reset} className="reset-button">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
