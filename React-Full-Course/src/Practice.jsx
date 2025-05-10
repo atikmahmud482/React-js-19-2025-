4d52d46c1182a1a3b74063208bff3f45c36c4339
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-10 23:24:53 +0600

    function components

--- React-Full-Course/src/Practice.jsx
+++ React-Full-Course/src/Practice.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Practice() {
-  return <div>Practice</div>;
+function Practice(props) {
+  return (
+    <div>
+      <h1>Hello ,{props.name}</h1>
+    </div>
+  );
 }
 
 export default Practice;
