15484abae164a3713f0e88d56269bd71438fe01c
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-10 23:29:58 +0600

    arrow function

--- React-Full-Course/src/Practice.jsx
+++ React-Full-Course/src/Practice.jsx
@@ -1,11 +1,20 @@
+// import React from "react";
+
+// function Practice(props) {
+//   return (
+//     <div>
+//       <h1>Hello ,{props.name}</h1>
+//     </div>
+//   );
+// }
+
+// export default Practice;
+
 import React from "react";
 
-function Practice(props) {
-  return (
-    <div>
-      <h1>Hello ,{props.name}</h1>
-    </div>
-  );
+function Practice() {
+  const Welcome = ({ name }) => <h1>Hello, {name}</h1>;
+  return <Welcome name="Atik" />;
 }
 
 export default Practice;
